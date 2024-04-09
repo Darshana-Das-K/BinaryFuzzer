2e22a3f1114c3487253b7a4ec253bc4f1ccd2fe9
LIBNATK <libnakuriakoset@gmail.com>
2024-04-09 11:14:29 +0530

    Added id in meta section

--- fuzzer/fuzzer_03_04/handle_meta.py
+++ fuzzer/fuzzer_03_04/handle_meta.py
@@ -1,9 +1,10 @@
 def handle_meta(meta):
-    file_extension=None
     for key, value in meta.items():
         if key == 'file-extension':
             file_extension = value
         elif key == 'endian':
             endianness = value
+        elif key == 'id':
+            id = value
             
-    return endianness, file_extension
+    return endianness, file_extension,id
--- fuzzer/fuzzer_03_04/main.py
+++ fuzzer/fuzzer_03_04/main.py
@@ -23,14 +23,9 @@ print(data_tree)
 
 # Write to file
 
-for i in range(0, 100):
-    endianness, file_extension = handle_meta(data_tree['meta'])
-    expansion = handle_seq(data_tree['seq'], endianness, data_tree)
-    
-    # Determine the file path
-    filename = f"output{i}.{file_extension}" if file_extension else f"output{i}"
-    filepath = os.path.join(output_directory, filename)
-    
-    # Write the output to the file
-    with open(filepath, 'wb') as file:
-        file.write(expansion)
+for i in range(0,10):
+  endianness, file_extension,id = handle_meta(data_tree['meta'])
+  expansion=handle_seq(data_tree['seq'], endianness,data_tree)
+  if file_extension:
+      with open(f"{id}{i}.{file_extension}", 'wb') as file:
+          file.write(expansion)
